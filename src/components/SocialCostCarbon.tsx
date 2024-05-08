b00be6fff0f7446536a99446282c0a92538ba0bc
purav-parekh <purav98@gmail.com>
2024-05-08 11:54:37 -0400

    set social cost of carbon

--- src/components/SocialCostCarbon.tsx
+++ src/components/SocialCostCarbon.tsx
@@ -1,38 +1,18 @@
 import { Select, Space } from "antd";
-import NumberInput from "./NumberInput";
+import { useState } from "react";
+import { sccOptions } from "../data/Constants";
 
 function SocialCostCarbon() {
+	// eslint-disable-next-line
+	const [socialCost, setSocialCost] = useState<string>("none");
 	const onChange = (value: string) => {
+		setSocialCost(value);
 		console.log(`selected ${value}`);
 	};
 
-	const onSearch = (value: string) => {
-		console.log("search:", value);
-	};
-
-	// Filter `option.label` match the user type `input`
-	const filterOption = (input: string, option?: { label: string; value: string }) =>
-		(option?.label ?? "").toLowerCase().includes(input.toLowerCase());
-
-	const sccOptions = [
-		{ value: "none", label: "No carbon price" },
-		{ value: "low", label: "Low - $20 in 2024" },
-		{ value: "medium", label: "Medium - $66 in 2024" },
-		{ value: "high", label: "High - $198 in 2024" },
-	];
-
 	return (
 		<Space className="flex justify-center py-5">
-			<Select
-				showSearch
-				placeholder="Start Date"
-				optionFilterProp="children"
-				onChange={onChange}
-				onSearch={onSearch}
-				filterOption={filterOption}
-				options={sccOptions}
-			/>
-			<NumberInput addOn="years" min={15} max={40} />
+			<Select className="w-64" placeholder="Social Cost" onChange={onChange} options={sccOptions} />
 		</Space>
 	);
 }
---
+++ src/data/Constants.ts
@@ -0,0 +1,6 @@
+export const sccOptions = [
+	{ value: "none", label: "No carbon price" },
+	{ value: "low", label: "Low - $20 in 2024" },
+	{ value: "medium", label: "Medium - $66 in 2024" },
+	{ value: "high", label: "High - $198 in 2024" },
+];
